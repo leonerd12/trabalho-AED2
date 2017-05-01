511b66a42aa6fde66c3d3bf14041968dc0ba52a9
Henrique Wildes <henriquewildes@gmail.com>
2017-05-01 16:57:37 -0400

    Instanciando árvore com valores aleatórios
    Signed-off-by: Henrique Wildes <henriquewildes@gmail.com>

--- arvore.c
+++ arvore.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TAM_Q1 50
+#define TAM_Q2 1000000
+
 typedef struct arvore {
 	int dado;
 	struct arvore* esq;
@@ -28,52 +31,79 @@ void insereArvore(Arvore** arv, int dado) {
 	}
 }
 
-void percorrerArvore(Arvore* arv) {
-	if (arv != NULL) {
-
-		printf("%d ", arv->dado);
-		percorrerArvore(arv->esq);
-		percorrerArvore(arv->dir);
-
-	}
-}
-
 void arvorePreFixada(Arvore* arv) {
 	if (arv != NULL) {
 		printf("%d ", arv->dado);
-		percorrerArvore(arv->esq);
-		percorrerArvore(arv->dir);
+		arvorePreFixada(arv->esq);
+		arvorePreFixada(arv->dir);
 	}
 }
 
 void arvoreCentral(Arvore* arv) {
 	if (arv != NULL) {
-		percorrerArvore(arv->esq);
+		arvoreCentral(arv->esq);
 		printf("%d ", arv->dado);
-		percorrerArvore(arv->dir);
+		arvoreCentral(arv->dir);
 	}
 }
 
 void arvorePosFixada(Arvore* arv) {
 	if (arv != NULL) {
-		percorrerArvore(arv->esq);
-		percorrerArvore(arv->dir);
+		arvorePosFixada(arv->esq);
+		arvorePosFixada(arv->dir);
 		printf("%d ", arv->dado);
 	}
 }
 
-int main() {
-	int num;
-	Arvore* arv;
+void embaralhar(int vet[], int tam) {
+	int temp, novaPosicao;
 
-	arv = init();
-
-	for (int i = 0; i < 10; i++) {
-		scanf("%d", &num);
-		insereArvore(&arv, num);
+	// Gera vetor com valores de 1 à 'tam'
+	for (int i = 0; i < tam; i++) {
+		vet[i] = i+1;
 	}
 
-	printf("Arvore preenchida.\n");
-	percorrerArvore(arv);
+	// Embaralha os valores do vetor
+	for (int i = 0; i < tam; i++) {
+		novaPosicao = rand() % tam;
+		temp = vet[i];
+		vet[i] = vet[novaPosicao];
+		vet[novaPosicao] = temp;
+	}
+}
 
+void imprimeVetor(int vet[], int tam) {
+	for (int i = 0; i < tam; i++) {
+		printf("%d ", vet[i]);
+	}
+	printf("\n");
+}
+
+int main() {
+	int num;
+	int valores[TAM_Q1];
+	Arvore* arv;
+
+	embaralhar(valores, TAM_Q1);
+	arv = init();
+
+	// Insere valores do vetor na árvore
+	for (int i = 0; i < TAM_Q1; i++) {
+		insereArvore(&arv, valores[i]);
+	}
+
+	printf("\n>> Arvore preenchida com os valores na seguinte ordem:\n");
+	imprimeVetor(valores, TAM_Q1);
+
+	printf("\n>> Caminhamento em pre-ordem:\n");
+	arvorePreFixada(arv);
+	printf("\n");
+
+	printf("\n>> Caminhamento ordem:\n");
+	arvoreCentral(arv);
+	printf("\n");
+
+	printf("\n>> Caminhamento em pos-ordem:\n");
+	arvorePosFixada(arv);
+	printf("\n\n");
 }
